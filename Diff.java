18ed2cb7c24df0ce6fb1cb1869f72e6017eb9152
Atul Kumar Awasthi <atulreso1@gmail.com>
2021-06-02 13:21:22 +0530

    adding message in Sum.java

--- Diff.java
+++ Diff.java
@@ -2,7 +2,7 @@ public class Diff {
     public static void name(String[] args) {
         int a= 5;
         int b = 6;
-        System.out.println("sum " + a-b);
+        System.out.println("sum is" + a-b);
 
         
     }
