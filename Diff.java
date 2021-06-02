c5e78c28b3d1b453b10700198d5bbac3c784e354
Atul Kumar Awasthi <atulreso1@gmail.com>
2021-06-02 15:51:31 +0530

    change

--- Diff.java
+++ Diff.java
@@ -1,8 +1,8 @@
 public class Diff {
     public static void name(String[] args) {
         int a= 5;
-        int b = 6;
-        System.out.println("sum is" + a-b);
+        int b = 56;
+        System.out.println( a-b);
 
         
     }
